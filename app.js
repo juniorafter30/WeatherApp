e708f0f0b270c30b9aa6b9b8e74ca6c4e6cfc06b
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-01-18 16:50:56 +0100

    Images added

--- app.js
+++ app.js
@@ -19,18 +19,44 @@ let $city;
 let url;
 
 const getWeather = () => {
-    $city = 'Warszawa';
+    $city = input.value;
     url = apiLink + $city + apiKey + units;
 
     axios.get(url)
     .then(res => {
         console.log(res)
         temp.innerText = Math.floor(res.data.main.temp) + " °C";
-        humidity.innerText = res.data.main.humidity;
-        pressure.innerText = res.data.main.pressure;
+        humidity.innerText = res.data.main.humidity + '%';
+        pressure.innerText = res.data.main.pressure + ' hPa';
         cityName.textContent = res.data.name;
         weather.innerText = res.data.weather[0].main;
-        } )
-}
 
-getWeather();
+        const imageId = res.data.weather[0].id;
+        
+        if(imageId >= 200 && imageId <= 299){
+            photo.setAttribute('src', 'images/thunderstorm.png')
+        } else if(imageId >= 300 && imageId <= 399) {
+            photo.setAttribute('src', 'images/drizle.png')
+        }else if (imageId >= 500 && imageId <= 599) {
+            photo.setAttribute('src', 'images/rain.png')
+        }else if (imageId >= 600 && imageId <= 699) {
+            photo.setAttribute('src', 'images/ice.png')
+        }else if (imageId >= 700 && imageId <= 799) {
+            photo.setAttribute('src', 'images/fog.png')
+        }else if (imageId = 800 ){
+            photo.setAttribute('src', 'images/sun.png')  
+        }else if (imageIf >= 801 && imageId <= 899) {
+             photo.setAttribute('src', 'images/cloud.png')
+        }else {
+            photo.setAttribute('src', 'images/unknown.png')
+        };
+    
+        
+        } );
+
+
+    
+};
+
+btn.addEventListener('click', getWeather());
+
